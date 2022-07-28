71a4fc2502fca489d949d0432cdeef5a306aea3c
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2022-07-28 12:26:31 -0500

    final code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 module.exports = {
   /**
@@ -8,7 +8,49 @@ module.exports = {
    * This gives you an opportunity to extend code.
    */
 
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    const extensionService = strapi.service('plugin::graphql.extension');
+
+    extensionService.use(({ strapi }) => ({
+      typeDefs: `
+          type Query {
+              restaurants(
+                  filters: RestaurantFiltersInput
+                  pagination: PaginationArg = {}
+                  sort: [String] = []
+                  publicationState: PublicationState = LIVE
+                  ): RestaurantEntityResponseCollection  
+              }
+          `,
+      resolvers: {
+        Query: {
+          restaurants: {
+            resolve: async (parent, args, context) => {
+              const { toEntityResponseCollection } =
+                strapi.service('plugin::graphql.format').returnTypes;
+              const { transformArgs } = strapi.service('plugin::graphql.builders').utils;
+              const contentType = strapi.contentTypes['api::restaurant.restaurant'];
+
+              const transformedArgs = transformArgs(args, { contentType });
+
+              const data = await strapi.entityService.findMany(
+                'api::restaurant.restaurant',
+                transformedArgs
+              );
+
+              const response = toEntityResponseCollection(data, {
+                args: { transformedArgs, start: 0, limit: 10 },
+                resourceUID: contentType.uid,
+              });
+
+              console.log('##################', response, '##################');
+              return response;
+            },
+          },
+        },
+      },
+    }));
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
